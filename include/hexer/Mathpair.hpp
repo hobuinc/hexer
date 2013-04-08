d1125cc628a9fd3647e9f5496b7c243a03faa671
Howard Butler <hobu.inc@gmail.com>
2013-04-08 09:41:56 -0500

    update header

--- include/hexer/Mathpair.hpp
+++ include/hexer/Mathpair.hpp
@@ -1,3 +1,16 @@
+/*****************************************************************************
+
+    (c) 2013 Hobu, Inc. hobu.inc@gmail.com
+
+    This is free software; you can redistribute and/or modify it under the
+    terms of the GNU Lesser General Licence as published by the Free Software
+    Foundation. See the COPYING file for more information.
+
+    This software is distributed WITHOUT ANY WARRANTY and without even the
+    implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+*****************************************************************************/
+
 #ifndef INCLUDED_PSHAPE_POINT_HPP
 #define INCLUDED_PSHAPE_POINT_HPP
 
