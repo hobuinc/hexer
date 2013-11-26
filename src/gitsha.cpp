f45db53516f71cb08d02ecbfb3819b368dca72d6
Howard Butler <hobu.inc@gmail.com>
2013-11-26 10:12:26 -0600

    fix dos line endings in CMakeLists.txt

--- src/gitsha.cpp
+++ src/gitsha.cpp
@@ -1,3 +1,3 @@
 #include <hexer/gitsha.h>
-#define GIT_SHA1 "d4c3d72e60dc1171fe0c55ec06b81c90dfee13c4"
+#define GIT_SHA1 "d012fdf1c056386e69fb30debd338a2e7bc0d473"
 const char g_GIT_SHA1[] = GIT_SHA1;
