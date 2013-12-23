01f8a155f5a77ede98573686bad8994ef2fe30f0
Howard Butler <hobu.inc@gmail.com>
2013-12-23 14:13:53 -0600

    fix issues in #9, use -std=c++11 for clang

--- src/gitsha.cpp
+++ src/gitsha.cpp
@@ -1,3 +1,3 @@
 #include <hexer/gitsha.h>
-#define GIT_SHA1 "d012fdf1c056386e69fb30debd338a2e7bc0d473"
+#define GIT_SHA1 "3c19d8ca4d901a3ef0bdeba6c7d95d2814f5cf80"
 const char g_GIT_SHA1[] = GIT_SHA1;
