0370b0829e73c7b87ab55681b53a8321f6a7fd4b
Howard Butler <hobu.inc@gmail.com>
2014-07-14 15:33:16 -0500

    fix-ups in preparation for 1.3.0 release

--- src/gitsha.cpp
+++ src/gitsha.cpp
@@ -1,3 +1,3 @@
 #include <hexer/gitsha.h>
-#define GIT_SHA1 "2bc2d8b2c8428605e98b900f2aa74c4f6b0fee98"
+#define GIT_SHA1 "e1e7aa34ed38e3764595a28d94645ab28b0c2750"
 const char g_GIT_SHA1[] = GIT_SHA1;
