6d97f3286a8fc11d7b6d6416f0ccdf240dc49a42
Howard Butler <hobu@hobu.co>
2014-08-01 09:27:17 -0500

    update sha

--- src/gitsha.cpp
+++ src/gitsha.cpp
@@ -1,3 +1,3 @@
 #include <hexer/gitsha.h>
-#define GIT_SHA1 "e1e7aa34ed38e3764595a28d94645ab28b0c2750"
+#define GIT_SHA1 "8c3a0f529c25cd06397d8ab6ca5904807e7e7633"
 const char g_GIT_SHA1[] = GIT_SHA1;
