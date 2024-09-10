3bd7d9fc9c681292690789665038ade44b227d89
ibell13 <122644517+ibell13@users.noreply.github.com>
2024-09-10 15:02:42 -0400

    Create Path.cpp

---
+++ hexer/Path.cpp
@@ -0,0 +1,35 @@
+#include <hexer/Path.hpp>
+
+namespace hexer 
+{
+
+void Path::toWKT(std::ostream& output) const
+{
+    std::vector<Point> pts = points();
+
+    auto outputPoint = [&output](Point& p)
+    {
+        output << p.m_x << " " << p.m_y;
+    };
+
+    output << "(";
+
+    auto pi = pts.begin();
+    if (pi != pts.end())
+        outputPoint(*pi++);
+    for (; pi != pts.end(); ++pi)
+    {
+        output << ", ";
+        outputPoint(*pi);
+    }
+
+    output << ")";
+
+    std::vector<Path *> paths = subPaths();
+    for (auto p : paths)
+    {
+        output <<",";
+        p->toWKT(output);
+    }
+}
+} // namespace hexer
